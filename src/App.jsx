9e8e3bfd93ddb964852a62612dc638bd089864f9
devenamulshikder <devenamulshikder@gmail.com>
2024-03-16 01:55:04 +0600

    initially projects setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl text-red-800">Vite + React</h1>
+      <h4 className="text-primary">Hello World</h4>
+    </>
+  )
+}
+
+export default App
